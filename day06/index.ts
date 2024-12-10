43bb180ed0de324c534806f72c03e583ecd1a667
Niklas <me@sodapop.se>
2024-12-10 10:21:47 +0100

    Day 6 part 2

--- day06/index.ts
+++ day06/index.ts
@@ -1,3 +1,4 @@
+import { toNumber, without } from "lodash-es";
 import { getInput, trimSplit } from "../utils.ts";
 
 const grid = trimSplit(await getInput(import.meta.dir)).map((l) => l.split(""));
@@ -5,16 +6,39 @@ const grid = trimSplit(await getInput(import.meta.dir)).map((l) => l.split(""));
 type XY = [number, number];
 
 let guard: XY = [0, 0];
+let start: XY = [0, 0];
 let direction: XY = [0, -1];
 
 outer: for (let y = 0; y < grid.length; y++) {
   for (let x = 0; x < grid[0].length; x++) {
     if (grid[y][x] === "^") {
-      guard = [x, y];
+      start = [x, y];
 
       break outer;
     }
   }
+}
+
+function printGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    let line = "";
+    for (let x = 0; x < grid[0].length; x++) {
+      if (visited.has(k([x, y]))) {
+        line += "X";
+      } else {
+        line += grid[y][x];
+      }
+    }
+    console.log(line);
+  }
+}
+
+function k(ns: Array<number>) {
+  return ns.join(",");
+}
+
+function v(str: string) {
+  return str.split(",").map(toNumber);
 }
 
 function turn() {
@@ -31,14 +55,25 @@ function turn() {
   }
 }
 
-const positions = new Set<string>();
+function reset() {
+  guard = start;
+  direction = [0, -1];
+}
+
+function go(x: number, y: number) {
+  guard = [x, y];
+}
+
+const visited = new Set<string>();
+
+reset();
 
 while (true) {
   let [x, y] = guard;
   let [dx, dy] = direction;
   let [nx, ny] = [x + dx, y + dy];
 
-  positions.add(`${x},${y}`);
+  visited.add(k([x, y]));
 
   if (ny < 0 || ny >= grid.length || nx < 0 || nx >= grid[0].length) {
     break;
@@ -47,20 +82,46 @@ while (true) {
   if (grid[ny][nx] === "#") {
     turn();
   } else {
-    guard = [nx, ny];
+    go(nx, ny);
   }
 }
 
-// for (let y = 0; y < grid.length; y++) {
-//   let line = "";
-//   for (let x = 0; x < grid[0].length; x++) {
-//     if (positions.has(`${x},${y}`)) {
-//       line += "X";
-//     } else {
-//       line += grid[y][x];
-//     }
-//   }
-//   console.log(line);
-// }
+console.log("Part 1", visited.size); // 4967
 
-console.log("Part 1", positions.size);
+reset();
+
+const obsPositions = without(Array.from(visited.values()), k(guard)).map(v);
+let loops = 0;
+
+obsPositions.forEach(([opx, opy]) => {
+  reset();
+  const visitedWithDirection = new Set<string>();
+  grid[opy][opx] = "#";
+
+  while (true) {
+    let [x, y] = guard;
+    let [dx, dy] = direction;
+    let [nx, ny] = [x + dx, y + dy];
+
+    visitedWithDirection.add(k([x, y, dx, dy]));
+
+    if (ny < 0 || ny >= grid.length || nx < 0 || nx >= grid[0].length) {
+      break;
+    }
+
+    if (visitedWithDirection.has(k([nx, ny, dx, dy]))) {
+      loops++;
+      break;
+    }
+
+    if (grid[ny][nx] === "#") {
+      turn();
+    } else {
+      go(nx, ny);
+    }
+  }
+
+  grid[opy][opx] = ".";
+});
+
+console.log("Part 2", loops); // 1789
