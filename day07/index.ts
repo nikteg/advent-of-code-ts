373521b47ff17a02c13fa7db36783e9dbff99dac
Niklas <me@sodapop.se>
2024-12-13 00:26:28 +0100

    Fancy day 7

--- day07/index.ts
+++ day07/index.ts
@@ -9,35 +9,19 @@ const sumNums = trimSplit(await getInput(import.meta.dir)).map((l) => {
   return [sum, nums] as SumNums;
 });
 
-function calc(numbers: number[], currentResult: number = 0, index: number = 0): number[] {
-  if (index === numbers.length - 1) {
-    return [currentResult + numbers[index], currentResult * numbers[index]];
-  }
-
-  if (index === 0) {
-    return calc(numbers, numbers[index], index + 1);
-  }
-
-  return [
-    ...calc(numbers, currentResult + numbers[index], index + 1),
-    ...calc(numbers, currentResult * numbers[index], index + 1),
-  ];
-}
-
-function calc2(numbers: number[], currentResult: number = 0, index: number = 0): number[] {
-  if (index === numbers.length - 1) {
-    return [currentResult + numbers[index], currentResult * numbers[index], concat(currentResult, numbers[index])];
-  }
-
-  if (index === 0) {
-    return calc2(numbers, numbers[index], index + 1);
-  }
-
-  return [
-    ...calc2(numbers, currentResult + numbers[index], index + 1),
-    ...calc2(numbers, currentResult * numbers[index], index + 1),
-    ...calc2(numbers, concat(currentResult, numbers[index]), index + 1),
-  ];
+function isValid(
+  numbers: number[],
+  total: number,
+  part2 = false,
+  currentResult: number = numbers[0],
+  index: number = 1,
+): boolean {
+  return index === numbers.length
+    ? currentResult === total
+    : (index === numbers.length && currentResult === total) ||
+        isValid(numbers, total, part2, currentResult + numbers[index], index + 1) ||
+        isValid(numbers, total, part2, currentResult * numbers[index], index + 1) ||
+        (part2 && isValid(numbers, total, part2, concat(currentResult, numbers[index]), index + 1));
 }
 
 function concat(a: number, b: number) {
@@ -46,10 +30,10 @@ function concat(a: number, b: number) {
 
 console.log(
   "Part 1",
-  sumBy(sumNums, ([s, nums]) => (calc(nums).some((n) => n === s) ? s : 0)),
+  sumBy(sumNums, ([s, nums]) => (isValid(nums, s, false) ? s : 0)),
 ); // 8401132154762
 
 console.log(
   "Part 2",
-  sumBy(sumNums, ([s, nums]) => (calc2(nums).some((n) => n === s) ? s : 0)),
+  sumBy(sumNums, ([s, nums]) => (isValid(nums, s, true) ? s : 0)),
 ); // 95297119227552
