b1d9637beb661a1dc4461747e972067ee2bc04c3
Niklas <me@sodapop.se>
2024-12-14 18:00:25 +0100

    Day 8 part 2

--- day08/index.ts
+++ day08/index.ts
@@ -1,4 +1,3 @@
-import { toNumber } from "lodash-es";
 import { getInput, trimSplit } from "../utils.ts";
 
 type XY = [number, number];
@@ -11,7 +10,7 @@ function isAntenna(c: string) {
 
 const frequencies = new Map<string, Array<XY>>();
 
-const update = (c: string, x: number, y: number) => {
+const addAntenna = (c: string, x: number, y: number) => {
   const existing = frequencies.get(c);
 
   if (existing) {
@@ -28,7 +27,7 @@ function distance([ax, ay]: XY, [bx, by]: XY): XY {
 grid.forEach((row, y) => {
   row.forEach((c, x) => {
     if (isAntenna(c)) {
-      update(c, x, y);
+      addAntenna(c, x, y);
     }
   });
 });
@@ -37,45 +36,41 @@ function k(coords: XY): string {
   return coords.join(",");
 }
 
-function v(str: string): XY {
-  return str.split(",").map(toNumber) as XY;
+function withinBounds([x, y]: XY) {
+  return x >= 0 && y >= 0 && x < grid[0].length && y < grid.length;
 }
 
-const antinodes = new Set<string>();
+function getAntinodeCount(part2 = false) {
+  const antinodes = new Set<string>();
 
-frequencies.entries().forEach(([c, antennas]) => {
-  antennas.forEach(([x, y], i) => {
-    antennas.forEach(([x2, y2], i2) => {
-      // is same antenna
-      if (i === i2) {
-        return;
-      }
+  frequencies.entries().forEach(([c, antennas]) => {
+    antennas.forEach(([x, y], i) => {
+      antennas.forEach(([x2, y2], i2) => {
+        // is same antenna
+        if (i === i2) {
+          return;
+        }
 
-      // is antinode
-      const [dx, dy] = distance([x, y], [x2, y2]);
-      const [nx, ny] = [x + dx, y + dy];
+        const [dx, dy] = distance([x, y], [x2, y2]);
 
-      // console.log(c, [x, y], [x2, y2], [dx, dy], [nx, ny]);
+        // don't include antenna itself in part 1
+        let [nx, ny] = part2 ? [x, y] : [x + dx, y + dy];
 
-      if (nx < 0 || ny < 0 || nx >= grid[0].length || ny >= grid.length) {
-        return;
-      }
+        while (withinBounds([nx, ny])) {
+          antinodes.add(k([nx, ny]));
+          [nx, ny] = [nx + dx, ny + dy];
 
-      antinodes.add(k([nx, ny]));
+          // if part 1, only include the first antinode
+          if (!part2) {
+            break;
+          }
+        }
+      });
     });
   });
-});
 
-grid.forEach((row, y) => {
-  let line = "";
-  row.forEach((c, x) => {
-    if (antinodes.has(k([x, y]))) {
-      line += "#";
-    } else {
-      line += c;
-    }
-  });
-  console.log(line);
-});
+  return antinodes.size;
+}
 
-console.log("Part 1", antinodes.size); // 276
+console.log("Part 1", getAntinodeCount()); // 276
+console.log("Part 2", getAntinodeCount(true)); // 991
